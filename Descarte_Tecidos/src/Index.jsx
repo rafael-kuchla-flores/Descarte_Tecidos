b3e112e4f278ddd8b38915da464a2673edec146f
rafael-kuchla-flores <rafael.kuchla.flores@gmail.com>
2026-08-13 10:50:29 -0300

    First Commit

---
+++ Descarte_Tecidos/src/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+      <header>
+        <h1>Projeto</h1>
+        <p>Descarte de tecidos</p>
+      </header>
+    </>
+  )
+}
+
+export default Index
