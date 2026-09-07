bd121e9876b8b41925d5dacd51c643f855ed1497
rafael-kuchla-flores <rafael.kuchla.flores@gmail.com>
2026-09-07 12:32:06 -0300

    feat(auth): add register component state and imports

--- Descarte_Tecidos/src/pages/auth/register/Register.jsx
+++ Descarte_Tecidos/src/pages/auth/register/Register.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
-import Header from '../../../components/header/Header'
+import React, { useState } from 'react'
+import Header from '../../../components/Header/Header'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { FiEye, FiEyeOff } from 'react-icons/fi'
+import plantalogin from '../../../assets/icons/plantalogin.png'
+import CustomInput from '../../../components/custominput/CustomInput'
+import authService from '../../../services/authService'
+import { RiRecycleFill } from 'react-icons/ri'
 const Register = () => {
+  const navigate = useNavigate()
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [confirmarSenha, setConfirmarSenha] = useState("")
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+
+
+
+
+
+
+
+
   return (
     <>
       <Header />
