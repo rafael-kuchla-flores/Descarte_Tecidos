824fcdaf9f5fdf8bf3b8bdd11791709dfcd685c5
AlanMatheus <matheusalan334@gmail.com>
2026-09-12 17:37:01 -0300

    ajuste de rota-admin

--- Descarte_Tecidos/src/routes/AppRoutes.jsx
+++ Descarte_Tecidos/src/routes/AppRoutes.jsx
@@ -35,6 +35,7 @@ function AppRoutes() {
       <Route path="/redefinir-senha/nova" element={<NewPassword />} />
 
       {/* admin */}
+      <Route element={<ProtectedRoute adminOnly={true} />}>
         <Route element={<AdminLayout />}>
           <Route path="/admin" element={<DashBoard />} />
           <Route path="/admin/campaigns" element={<CampaignsAdmin />} />
@@ -42,7 +43,6 @@ function AppRoutes() {
           <Route path="/admin/users" element={<UsersAdmin />} />
           <Route path="/admin/content" element={<ContentAdmin />} />
         </Route>
-      <Route element={<ProtectedRoute adminOnly={true} />}>
       </Route>
       <Route path="/acesso-negado" element={<AccessDenied />} />
     </Routes>
