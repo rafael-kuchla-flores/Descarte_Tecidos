5c3b54d101bfcf3f2a746bb0cc5881e1b5399e24
AlanMatheus <matheusalan334@gmail.com>
2026-09-18 13:15:15 -0300

    feat/criar layout de perfil-gerente

--- Descarte_Tecidos/src/routes/AppRoutes.jsx
+++ Descarte_Tecidos/src/routes/AppRoutes.jsx
@@ -19,6 +19,9 @@ import UsersAdmin from "../pages/admin/users/Users.jsx";
 import ContentAdmin from "../pages/admin/content/ContentAdmin.jsx";
 //acess denied
 import AccessDenied from "../pages/access-denied/AccessDenied.jsx";
+// Manager
+import ManagerLayout from "../components/managerlayout/ManagerLayout.jsx";
+
 
 function AppRoutes() {
   return (
@@ -45,6 +48,13 @@ function AppRoutes() {
         </Route>
       </Route>
       <Route path="/acesso-negado" element={<AccessDenied />} />
+
+      {/* Manager */}
+      <Route element={<ProtectedRoute managerOnly={true} />}>
+        <Route element={<ManagerLayout />}>
+          
+        </Route>
+      </Route>
     </Routes>
   );
 }
